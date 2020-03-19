1939b5a4cd8db549af83910d47e29fcfcdb0685a
Phillwich <P.Hellwich@gmx.de>
2020-03-19 16:20:03 +0100

    check if path is the same

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container fluid>
-        <v-toolbar >
-            <v-toolbar-title>FinanceApp</v-toolbar-title>
+        <v-toolbar>
+            <v-toolbar-title @click="pushMe('/')">FinanceApp</v-toolbar-title>
             <v-spacer></v-spacer>
             <div v-if="!isLoggedIn" class="pr-2">
                 <v-btn @click="pushMe('/auth/login')">Loggin</v-btn>
@@ -9,8 +9,11 @@
             <div v-if="!isLoggedIn">
                 <v-btn @click="pushMe('/auth/register')">Sign up</v-btn>
             </div>
+            <div class="pr-2" v-if="isLoggedIn">
+                <v-btn @click="pushMe('/overview')">My Overview</v-btn>
+            </div>
             <div v-if="isLoggedIn">
-                <v-btn @click="pushMe('/')">Log out</v-btn>
+                <v-btn @click="Logout()">Log out</v-btn>
             </div>
         </v-toolbar>
     </v-container>
@@ -20,13 +23,18 @@
 import { mapState, mapActions } from 'vuex'
 export default {
     computed: {
-        ...mapState(['isLoggedIn'])
+        ...mapState(['isLoggedIn','user'])
     },
     methods: {
         ...mapActions(['logoutUser']),
         pushMe(value){
-            this.$router.push(value)
-            if (value === '/') return this.logoutUser()
+            if (value === '/overview') value += `/${this.user.userId}`
+            if (this.$route.path !== value) this.$router.push(value)
+            
+        },
+        Logout(){
+            this.pushMe('/')
+            return this.logoutUser()
         }
     }
     
